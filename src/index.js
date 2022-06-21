b96ff47759a1c3f9447f3287e2b1c331599c148d
obf1313 <oubaifan@163.com>
2022-06-21 10:02:04 +0800

    校验是否包含 emoji

--- src/index.js
+++ src/index.js
@@ -72,6 +72,16 @@ function hasWhiteSpace(str = '') {
   return whiteSpaceReg.test(str);
 }
 
+/**
+ * @descriptor 是否包含 emoji
+ * @param str 
+ * @returns boolean
+ */
+function hasEmoji(str = '') {
+  const emojiReg = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\ufe0f|\u200d|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+  return emojiReg.test(str);
+}
+
 export {
   isTFN,
   isEmail,
@@ -79,5 +89,6 @@ export {
   isIdNumber,
   isURL,
   isPassword,
-  hasWhiteSpace
+  hasWhiteSpace,
+  hasEmoji
 };
